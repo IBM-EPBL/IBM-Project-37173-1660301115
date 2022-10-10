d1f303487e773a1a401b89be5a78f03ed4ebb3c1
Edwin-francis <90142492+Edwin-francis@users.noreply.github.com>
2022-10-10 15:17:32 +0530

    Create ass1.py

---
+++ assng-1/team member/Signs with Smart Connectivity for Better Road Safety/ass1.py
@@ -0,0 +1,18 @@
+void setup()
+{
+
+pinMode(3, INPUT); pinMode(13, OUTPUT);
+
+}
+
+void loop()
+
+{
+If (digitalRead(3) == HIGH) { digitalWrite(13, HIGH);
+} else {
+digitalWrite(13, LOW);
+}
+delay(10); // Delay a little bit to improve simulation performance 
+}
+
+
